0162ec5eb1be7550395addd7a7beeb363379d521
Eli Montoya <elmo9257@colorado.edu>
2023-02-06 19:32:24 -0600

    replaced incorrectly used deleted User model in routes with YourCustomModel

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { YourCustomModel } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await YourCustomModel.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await YourCustomModel.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
